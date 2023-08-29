ea88f3a313600884b44186ded1b9ac4c782e36c1
poya0a <damdadodam1@gmail.com>
2023-08-29 17:59:17 +0900

    수정

--- src/components/common/media/MediaList.tsx
+++ src/components/common/media/MediaList.tsx
@@ -1,43 +1,27 @@
-import React from 'react';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import MediaItem from './MediaItem';
-import Slider from "react-slick";
+import React from "react";
+import MediaItem from "./MediaItem";
 
 const MediaList = () => {
-    const settings = {
-          infinite: false, 
-          speed: 1200, 
-          slidesToShow: 5, 
-          slidesToScroll: 1, 
-          centerMode: true, 
-          centerPadding: '10px',
-          autoplay: false, 
-          autoplaySpeed: 2500,
-          dots: false,
-        };
-    return (
-        <div className="media_list">
-            <div className="slide_wrap">
-                <div className="slide_container">
-                    <ul className="list">
-                        <Slider {...settings}>
-                            <MediaItem/>
-                            <MediaItem/>
-                            <MediaItem/>
-                            <MediaItem/>
-                            <MediaItem/>
-                            <MediaItem/>
-                            <MediaItem/>
-                            <MediaItem/>
-                            <MediaItem/>
-                            <MediaItem/>
-                        </Slider>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="media_list">
+      <div className="slide_wrap">
+        <div className="slide_container">
+          <ul className="list">
+            <MediaItem />
+            <MediaItem />
+            <MediaItem />
+            <MediaItem />
+            <MediaItem />
+            <MediaItem />
+            <MediaItem />
+            <MediaItem />
+            <MediaItem />
+            <MediaItem />
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MediaList;
